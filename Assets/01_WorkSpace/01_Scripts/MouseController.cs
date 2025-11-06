a5dda153160a9f859f9f2cc4fe4e7b3873031c37
Mizuyaki <dungvu3104@gmail.com>
2025-11-06 21:59:48 +0700

    part 3.3 ok

--- Assets/01_WorkSpace/01_Scripts/MouseController.cs
+++ Assets/01_WorkSpace/01_Scripts/MouseController.cs
@@ -8,24 +8,76 @@ public class MouseController : MonoBehaviour
     private Rigidbody2D playerRigidbody;
     public float forwardMovementSpeed = 3.0f;
 
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+    private Animator mouseAnimator;
+
+    public ParticleSystem jetpack;
+
+    private bool isDead = false;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-
+        mouseAnimator = GetComponent<Animator>();
     }
+
     void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
+        jetpackActive = jetpackActive && !isDead;
+
         if (jetpackActive)
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
 
-        Vector2 newVelocity = playerRigidbody.velocity;
-        newVelocity.x = forwardMovementSpeed;
-        playerRigidbody.velocity = newVelocity;
+        if (!isDead)
+        {
+            Vector2 newVelocity = playerRigidbody.velocity;
+            newVelocity.x = forwardMovementSpeed;
+            playerRigidbody.velocity = newVelocity;
+        }
 
+        UpdateGroundedStatus();
+        AdjustJetpack(jetpackActive);
     }
+
+    void UpdateGroundedStatus()
+    {
+        //1
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        //2
+        mouseAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        var jetpackEmission = jetpack.emission;
+        jetpackEmission.enabled = !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackEmission.rateOverTime = 300.0f;
+        }
+        else
+        {
+            jetpackEmission.rateOverTime = 75.0f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HitByLaser(collider);
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+        isDead = true;
+        mouseAnimator.SetBool("isDead", true);
+    }
+
 }
