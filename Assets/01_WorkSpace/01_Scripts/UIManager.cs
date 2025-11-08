eec43ca81d77f950a289e1083ab56e909dccaa83
Mizuyaki <dungvu3104@gmail.com>
2025-11-08 22:06:04 +0700

    done Unity UI part 2

--- Assets/01_WorkSpace/01_Scripts/UIManager.cs
+++ Assets/01_WorkSpace/01_Scripts/UIManager.cs
@@ -5,9 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+
+    public Animator dialog;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 
 
